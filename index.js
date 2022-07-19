058a5da3758465040cc27100b2a37820569dd89b
Boy Baukema <>
2022-07-19 14:48:28 +0200

    Add fix

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ app.get('/', (req, res) => {
   res.send(`{"username": "${req.query.username}"}`)
 })
 
+app.get('/fixed', (req, res) => {
+  res.header("Content-Type", "application/json");
+  res.send(`{"username": "${req.query.username}"}`)
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
   exec('open http://localhost:3000/?username=World%3Cscript%3Ealert%28%27xss%27%29%3B%3C%2Fscript%3E')
